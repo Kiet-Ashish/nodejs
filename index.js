fa658fe779ef9d3d33324734c8b7bc3533c97fe2
Kiet-Ashish <101831514+Kiet-Ashish@users.noreply.github.com>
2023-11-04 15:09:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const http = require('http');
-const PORT = 3000;
+const express = require("express");
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.set('views', './views');
+app.set("view engine", "ejs");
+const indexRoute = require("./routes/index");
+const educationRoute = require("./routes/education");
+const experienceRoute = require("./routes/experience");
+const projectRoute = require("./routes/project");
+const contactRoute = require("./routes/contact");
+
+
+app.use('/education', educationRoute);
+app.use('/experience', experienceRoute);
+app.use('/project', projectRoute);
+app.use('/contact', contactRoute);
+app.use('/', indexRoute);
+
+app.use((req, res, next) =>{
+    res.status(404).render('404', {
+        home: false,
+        education: false,
+        experience: false,
+        contact: false,
+        project: false
+    });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+
+app.listen(process.env.PORT || 3000);
+
+
